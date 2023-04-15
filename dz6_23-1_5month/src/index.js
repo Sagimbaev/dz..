f848be71d5611cc30472ddbfad7c4facf658e163
Sagimbaev <115146538+Sagimbaev@users.noreply.github.com>
2023-04-15 16:41:17 +0600

    Add files via upload

---
+++ dz6_23-1_5month/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+import {ApiProvider} from "@reduxjs/toolkit/dist/query/react";
+import {dataSlice} from "./features/DataSlice";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+      <ApiProvider api={dataSlice}>
+          <App />
+      </ApiProvider>
+  </React.StrictMode>
+);
+
+
